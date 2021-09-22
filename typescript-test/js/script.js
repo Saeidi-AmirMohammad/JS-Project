a6bf28569ecdaaa64247bf8c6280817786e708d5
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-09-22 23:20:54 +0330

    DataTypeInTypeScript

--- typescript-test/js/script.js
+++ typescript-test/js/script.js
@@ -1,1 +1,5 @@
-console.log('SaeidiAmirMohammad');
+"use strict";
+var fullname = 'SaeidiAmirMohammad';
+var num = 12;
+var bool = false;
+var list = ['ahamd', 'reza', 'amir'];
--- typescript-test/js/script.ts
+++ typescript-test/js/script.ts
@@ -1,1 +1,5 @@
-console.log('SaeidiAmirMohammad');
+let fullname : string = 'SaeidiAmirMohammad';
+let num : number = 12;
+let bool : boolean = false;
+let list = ['ahamd' , 'reza' , 'amir'];
+
