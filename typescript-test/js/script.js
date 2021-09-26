3e4f88dab73d96932ebb1a5e4c040f7dfc49d6fd
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-09-26 22:45:16 +0330

    ObjectInTypeScript

--- typescript-test/js/script.js
+++ typescript-test/js/script.js
@@ -1,13 +1,7 @@
 "use strict";
-function sum(x, y) {
-    return x + y;
-}
-var sum2 = function (x, y) {
-    return x + y;
-};
-var sum3 = function (x, y) { return x + y; };
-console.log(sum(3, 7));
-var sum4;
-sum4 = function (x, y) {
-    return x + y;
+var person = {
+    fullname: 'AmirSaeidi',
+    age: function (num) {
+        return "age : " + num;
+    }
 };
--- typescript-test/js/script.ts
+++ typescript-test/js/script.ts
@@ -1,15 +1,11 @@
-function sum(x : number , y : number) : number{
-    return x + y;
+type MyObject = {
+    fullname : string,
+    age : (num : number) => string
 }
 
-let sum2 = function(x : number , y : number) : number{
-    return x + y;
-}
-
-let sum3 = (x : number , y : number) : number => x + y
-console.log(sum (3 , 7))
-
-let sum4 : (x : number , y : number) => number
-sum4 = function(x : number , y : number){
-return x + y
-}
+let person : MyObject = {
+    fullname : 'AmirSaeidi',
+    age : (num : number) => {
+        return `age : ${num}`
+    }
+};
