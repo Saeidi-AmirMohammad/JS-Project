94c7c896e34bf48346b4ea42b913c5df05ff6d94
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-09-27 00:16:32 +0330

    Set,GetInTypeScript

--- typescript-test/js/script.js
+++ typescript-test/js/script.js
@@ -4,7 +4,22 @@ var Car = /** @class */ (function () {
         this.model = model,
             this.year = year;
     }
+    Object.defineProperty(Car.prototype, "setModel", {
+        set: function (theModel) {
+            this.model = theModel;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Car.prototype, "getModel", {
+        get: function () {
+            return this.model;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Car;
 }());
 var car01 = new Car('x5', 2020);
-console.log(car01);
+car01.setModel = 'x4';
+console.log(car01.getModel);
--- typescript-test/js/script.ts
+++ typescript-test/js/script.ts
@@ -1,13 +1,22 @@
 class Car {
-    model : string
-    year : number
+   protected model : string
+public year : number
 
     constructor(model : string , year : number){
         this.model = model,
         this.year = year
 
     }
+
+    set setModel(theModel : string){
+        this.model = theModel;
+    }
+
+    get getModel(){
+        return this.model;
+    }
 }
 
 let car01 = new Car('x5' , 2020)
-console.log(car01)
+car01.setModel = 'x4';
+console.log(car01.getModel);
