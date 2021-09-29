0ad3f3929178d6cb7f6c2a78ffcd6f3ab634b379
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-09-29 21:54:27 +0330

    GenericInTypeScript

--- typescript-test/js/script.js
+++ typescript-test/js/script.js
@@ -1,9 +1,5 @@
 "use strict";
-function printLabel(labelObj) {
-    console.log(labelObj.label);
+function logData(data) {
+    return data;
 }
-function labelX(labelObj) {
-    console.log(labelObj.size);
-}
-var myObj = { size: 10, label: 'Size 7', number: 123 };
-var myObj2 = { size: 15, label: 'Size 10' };
+console.log(logData(['1', 'Saeidi']));
--- typescript-test/js/script.ts
+++ typescript-test/js/script.ts
@@ -1,16 +1,5 @@
-interface Mylabel {
-    size : number
-    label : string
+function logData<T>(data : T[]) : T[] {
+return data;
 }
 
-function printLabel(labelObj : Mylabel){
-    console.log(labelObj.label)
-}
-
-function labelX(labelObj : Mylabel){
-    console.log(labelObj.size)
-}
-
-let myObj = {size : 10 , label : 'Size 7' , number : 123}
-let myObj2 = {size : 15 , label : 'Size 10'}
-
+console.log(logData<string>(['1' , 'Saeidi']));
