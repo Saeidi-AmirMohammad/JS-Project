f1b784698f2d15d0127cf910c6ec9fc206fa65b7
SaeidiAmirMohammad <amirmohammad1379.saeidi@gmail.com>
2021-09-30 18:59:49 +0330

    GenericWithClassInTypeScript

--- typescript-test/js/script.js
+++ typescript-test/js/script.js
@@ -1,5 +1,17 @@
 "use strict";
-function logData(data) {
-    return data;
-}
-console.log(logData(['1', 'Saeidi']));
+var myArry = /** @class */ (function () {
+    function myArry(data, data2) {
+        this.data = data;
+        this.data2 = data2;
+    }
+    myArry.prototype.addItem = function (item) {
+        this.data.push(item);
+    };
+    myArry.prototype.getItem = function (index) {
+        return this.data[index];
+    };
+    return myArry;
+}());
+var list = new myArry(['item1', 'item2'], [true, false]);
+list.addItem('4');
+console.log(list.getItem(0));
--- typescript-test/js/script.ts
+++ typescript-test/js/script.ts
@@ -1,5 +1,14 @@
-function logData<T>(data : T[]) : T[] {
-return data;
+class myArry<T extends number | string , U>{
+constructor(public data : T[] , private data2 : U[]){}
+    addItem(item : T){
+        this.data.push(item)
+    }
+
+    getItem(index : number){
+        return this.data[index]
+    }
 }
 
-console.log(logData<string>(['1' , 'Saeidi']));
+let list = new myArry<string , boolean>(['item1' , 'item2'] , [true , false]);
+list.addItem('4');
+console.log(list.getItem(0))
